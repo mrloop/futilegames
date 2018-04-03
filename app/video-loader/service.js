5513d290b497069f1b76ccd1810a2ea6bee0a757
mrloop <ewan@mrloop.com>
2018-04-03 22:08:58 +0100

    Stagger fetch request of Chrome fails
    Chrome runs out of resources when too many concurrent fetch requests so
    queue them.

--- app/video-loader/service.js
+++ app/video-loader/service.js
@@ -6,12 +6,31 @@ export default Service.extend({
   assetMap: service('asset-map'),
 
   preload(format) {
-    debug(format);
-    Object.values(this.get('assetMap.map')).forEach((url) => {
-      if (url.split(".").pop() === format) {
-        debug(url);
-        fetch(new Request(url, { mode: 'cors' }));
-      }
+    debug(`Loading: ${format}`);
+    const arr = Object.values(this.get('assetMap.map')).filter((url) => {
+      return url.split(".").pop() === format;
     });
+    let it = this.iterator(arr);
+    this.fetchNext(it);
+  },
+
+  fetchNext(it) {
+    const i = it.next();
+    if (!i.done) {
+      fetch(new Request(i.value, { mode: 'cors' }))
+        .then(() => this.fetchNext(it))
+        .catch(() => this.fetchNext(it));
+    }
+  },
+
+  iterator(array) {
+    let nextIndex = 0;
+    return {
+      next() {
+        return nextIndex < array.length ?
+        { value: array[nextIndex++], done: false } :
+        { done: true };
+      }
+    }
   },
 });
