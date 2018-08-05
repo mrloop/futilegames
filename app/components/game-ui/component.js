6cf8364ef97a8276441b30bfbb649af27c99d70a
mrloop <ewan@mrloop.com>
2018-08-05 20:54:40 +0100

    Add drinks glasses paths

--- app/components/game-ui/component.js
+++ app/components/game-ui/component.js
@@ -1,8 +1,9 @@
 import Component from "@ember/component";
 
 export default Component.extend({
-  attributeBindings: ["xmlns"],
+  attributeBindings: ["xmlns", "viewBox"],
   xmlns: "http://www.w3.org/2000/svg",
   tagName: "svg",
   classNames: "game-ui",
+  viewBox: "0 0 788 576",
 });
